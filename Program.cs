90f671ccaae2a6557c13be7924c09f26f350be61
Crocodile <wwwblogger1@gmail.com>
2023-02-08 19:24:12 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Lesson
+
+{
+    /*
+     * ключевое слово ref
+     */
+    /*
+     * передача аргументов по ссылке
+     */
+
+    class Program
+    {
+        static void Foo(int a)
+        {
+            a = -5;
+        }
+
+
+
+        static void Main(string[] args)
+
+        {
+            int a = 2;
+            Foo(a);
+
+            Console.WriteLine(a);
+        }
+
+        // данные с метода Main скопируются в метод Foo 
+        /*  class Program
+          {
+              static void Foo(ref int a, ref int b, ref int c, ref int d)
+              {
+                  a = -5;
+              }
+
+
+
+              static void Main(string[] args)
+
+              {
+                  int a = 2;
+                  Foo(ref a);
+
+                  Console.WriteLine(a);
+            */
+
+        // благодаря ключевому слову ref используя ЗНАЧИМЫЙ ТИП мы получили поведение как у ССЫЛОЧНЫХ ТИПОВ
+
+    }
+}
+
+
+
+
+
+
